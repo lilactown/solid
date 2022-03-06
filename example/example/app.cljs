37ac06a5ed31d61db9c396642b82bed50f205eeb
Will Acton <will@willacton.com>
2022-03-06 10:36:10 -0800

    differentiate between components and dom els

--- example/example/app.cljs
+++ example/example/app.cljs
@@ -1,17 +1,34 @@
 (ns example.app
   (:require
+   [goog.object :as gobj]
    [town.lilac.solid.core :as s]
-   [town.lilac.solid.web :as web :refer [$]]))
+   [town.lilac.solid.web :as web :refer [$d $c]]))
+
+
+(defn counter
+  [props]
+  (let [initial-count (:initial-count props)
+        [count set-count] (s/signal initial-count)]
+    (s/effect #(set-count (:initial-count props)))
+    ($d "div"
+        "Count: "
+        count
+        ($d "button"
+            #js {:onClick (fn [_] (set-count inc))}
+            "+"))))
 
 
 (defn app
   []
-  (let [[count set-count] (s/signal 0)]
-    ($ "div"
-       count
-       ($ "button"
-          #js {:onClick (fn [_] (set-count inc))}
-          "hi"))))
+  (let [[initial-count set-initial-count] (s/signal 4)]
+    ($d "div"
+        "Starting count: "
+        ($d "input" #js {:onChange (fn [e]
+                                     (set-initial-count
+                                      (js/parseInt (.. e -target -value))))
+                         :value initial-count
+                         :type "number"})
+        ($c counter #js {:initial-count initial-count}))))
 
 
 (defonce cleanup identity)
--- src/town/lilac/solid/web.cljs
+++ src/town/lilac/solid/web.cljs
@@ -1,10 +1,17 @@
 (ns town.lilac.solid.web
   (:require
    ["solid-js/web" :as web]
-   ["solid-js/h" :as h]))
+   ["solid-js/h" :as h]
+   [cljs-bean.core :as b]))
 
 
 (def render web/render)
 (def hydrate web/hydrate)
 
-(def $ h)
+(def $d h)
+
+(defn $c
+  [c & rest]
+  (let [c' (fn [props]
+             (c (b/bean props)))]
+    (apply h c' rest)))
